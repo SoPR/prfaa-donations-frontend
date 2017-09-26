071f3927782032d77faef6f5579dade6c7140051
Jeff Ahking <wolphox@gmail.com>
2017-09-26 19:42:44 -0400

    add react router, add mock landing page and thankyou page, add submission behavior

--- src/DonationForm/actions/donationFormActions.js
+++ src/DonationForm/actions/donationFormActions.js
@@ -1,14 +1,14 @@
 import client from '../../Feathers';
 
 const donationSubmitted = new Event('donationSubmitted');
-const submissionError   = new Event('submissionError');
+const submissionError = new Event('submissionError');
 
 const submitFormRequest = (data) => {
     const donationOfferService = client.service('donation-offer');
     donationOfferService.create(data)
         .then((res) => {
     document.dispatchEvent(donationSubmitted)
-            // Redirect user to Thank You
+    // Redirect user to Thank You
   })
         .catch((error) => {
     document.dispatchEvent(submissionError)
