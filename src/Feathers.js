ccc1ba8fc2c89d34cf228d14a1857a8ee1cef6b7
Neil A. Beardsley <nabeards@me.com>
2017-09-28 02:50:34 -0600

    Connects feathers to backend server.

--- src/Feathers.js
+++ src/Feathers.js
@@ -3,8 +3,9 @@ import feathers from 'feathers/client';
 import hooks from 'feathers-hooks';
 import socketio from 'feathers-socketio/client';
 
-const socket = io(process.env.REACT_APP_BACKEND_URL);
-const client = feathers();
+const serverAddy = process.env.REACT_APP_BACKEND_URL || 'http://donationbackendlb-1bjc8td-1643657227.us-east-1.elb.amazonaws.com';
+const socket     = io(serverAddy);
+const client     = feathers();
 
 client.configure(hooks());
 client.configure(socketio(socket, {timeout: 5000}));
