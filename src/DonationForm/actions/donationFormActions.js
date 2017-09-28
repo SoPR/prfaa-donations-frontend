ec0e918cbae9296e0602d82c2168b1ffdf3ddefd
Neil A. Beardsley <nabeards@me.com>
2017-09-28 00:41:37 -0600

    Adds backend URL for production.

--- src/DonationForm/actions/donationFormActions.js
+++ src/DonationForm/actions/donationFormActions.js
@@ -4,7 +4,8 @@ const donationSubmitted = new Event('donationSubmitted');
 const submissionError = new Event('submissionError');
 
 const submitFormRequest = (data) => {
-    axios.post(process.env.REACT_APP_BACKEND_URL + '/donation-offer/', data)
+    const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://donationbackendlb-1bjc8td-1643657227.us-east-1.elb.amazonaws.com/';
+    axios.post(backendUrl + '/donation-offer/', data)
     .then(function (response) {
     document.dispatchEvent(donationSubmitted)
     // Redirect user to Thank You
