e1f562e8951f4db23de3511469b7ea6d25bdc266
Neil A. Beardsley <nabeards@me.com>
2017-09-28 00:54:37 -0600

    Repair to prod link.

--- src/DonationForm/actions/donationFormActions.js
+++ src/DonationForm/actions/donationFormActions.js
@@ -4,7 +4,7 @@ const donationSubmitted = new Event('donationSubmitted');
 const submissionError = new Event('submissionError');
 
 const submitFormRequest = (data) => {
-    const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://donationbackendlb-1bjc8td-1643657227.us-east-1.elb.amazonaws.com/';
+    const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://donationbackendlb-1bjc8td-1643657227.us-east-1.elb.amazonaws.com';
     axios.post(backendUrl + '/donation-offer/', data)
     .then(function (response) {
     document.dispatchEvent(donationSubmitted)
