85d8ed33750509af66195e820fa5a7680fa1a593
Bryan Cruz <Bryan@innerbounce.com>
2017-09-28 17:47:57 -0400

    restructured the navbar with bootstrap-react

--- src/Layout/components/header.js
+++ src/Layout/components/header.js
@@ -11,18 +11,20 @@ export default class ThankYou extends Component {
   render() {
     return(
       <Navbar collapseOnSelect>
-        <Navbar.Brand>
-          <Link to='/'>Home</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to='/'>Home</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
         <Navbar.Collapse>
-        <Nav bsStyle="pills">
-          <li role="presentation">
-            <Link eventKey={1} to='/donation-form'>Donation form link</Link>
-          </li>
-          <li role="presentation">
-            <Link eventKey={2} to='/donation-form/thank-you'>Thank You page link</Link>
-          </li>
+        <Nav>
+          <NavItem>
+            <Link eventKey={1} to="/donation-form">Donation form link</Link>
+          </NavItem>
+          <NavItem>
+            <Link eventKey={2} to="/donation-form/thank-you">Thank You page link</Link>
+          </NavItem>
         </Nav>
         </Navbar.Collapse>
       </Navbar>
