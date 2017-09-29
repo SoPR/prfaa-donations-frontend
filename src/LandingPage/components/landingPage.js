07c860b90a842af985934d19dc902bc1f39ad872
Bryan Cruz <Bryan@innerbounce.com>
2017-09-29 00:32:06 -0400

    Began Styling the Homepage

--- src/LandingPage/components/landingPage.js
+++ src/LandingPage/components/landingPage.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import {
+  Grid,
+  Row,
+  Col,
+  Button
+} from 'react-bootstrap'
 
 export default class LandingPage extends Component {
   render() {
     return(
-      <div>
-        <h1>Landing Page Under Construction</h1>
-        <Link to='/donation-form'>Donation form link</Link>
-        <Link to='/donation-form/thank-you'>Thank You page link</Link>
-      </div>
+      <Grid fluid id="landing-page">
+        <Row>
+          <Col xs={12} mdOffset={2} md={8} className="text-center landing-text">
+            <h1>Welcome to the PRFAA Register a Donation Page</h1>
+            <h3>If you want to donate, click on the button bellow to fill a form with all the details!</h3>
+          </Col>
+        </Row>
+        <Row id="donate-button-conainer">
+          <Col xs={12} className="text-center">
+            <Button bsStyle="donate" bsSize="large" href='/donation-form'>Donation form link</Button>
+          </Col>
+        </Row>
+      </Grid>
+
     )
   }
 }
