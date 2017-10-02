0c397584fa94d38be69bffff3008f1a08396b653
Neil A. Beardsley <nabeards@me.com>
2017-10-02 00:48:07 -0600

    Prepwork for turning on authentication.

--- src/Feathers.js
+++ src/Feathers.js
@@ -1,24 +1,16 @@
 import feathers from 'feathers/client';
 import hooks from 'feathers-hooks';
-
-//import io from 'socket.io-client';
 import agent from 'superagent';
-
-//import socketio from 'feathers-socketio/client';
 import rest from 'feathers-rest/client';
-
-import config from './config';
+import auth from 'feathers-authentication-client';
+import { config } from './config';
 
 const serverAddy = config.backendUrl;
 const restful    = rest(serverAddy);
-//const socket     = io(serverAddy);
-
-//const client = feathers()
-//    .configure(hooks())
-//    .configure(socketio(socket, {timeout: 5000}));
 
 const client = feathers()
     .configure(hooks())
-    .configure(restful.superagent(agent));
+    .configure(restful.superagent(agent))
+    .configure(auth({storage: window.localStorage}));
 
 export default client;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-export default class config {
-    static get backendUrl() {
-        return process.env.REACT_APP_BACKEND_URL || 'http://donationbackendlb-1bjc8td-1643657227.us-east-1.elb.amazonaws.com';
-    }
+export const config = {
+    backendUrl: process.env.REACT_APP_BACKEND_URL || 'http://donationbackendlb-1bjc8td-1643657227.us-east-1.elb.amazonaws.com'
 }
