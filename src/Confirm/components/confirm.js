4567354e431c13a4190a56cfe1ec564edcd74743
Bryan Cruz <bryan@innerbounce.com>
2017-10-03 23:56:33 -0400

    Thank you page styling (#25)
    * Bootstraped the confirmation page
    
    * adding react-icons including font-awesome
    
    * setting footer top and bottom padding to 35px
    
    * Added green checkmark on confirm.js and go back home button
    
    * Added spinner on the confirm page
    
    * changing jumbotron backgroundcolor and border color
    
    * added svg icons on confirm.js file
    
    * Styled the login page
    
    * reformated login js file

--- src/Confirm/components/confirm.js
+++ src/Confirm/components/confirm.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import queryString from 'query-string';
 import client from '../../Feathers';
+import {
+    Grid,
+    Jumbotron,
+    Row,
+    Col
+} from 'react-bootstrap';
+import {
+    FaCheck,
+    FaSpinner
+} from 'react-icons/lib/fa';
+import GoX from 'react-icons/lib/go/x'
+
 
 export default class Confirm extends Component {
     constructor(...args) {
@@ -29,34 +41,46 @@ export default class Confirm extends Component {
         if (!this.state.confirmed && !this.state.error) {
             jsx = (
                 <div>
-                    <h1>Confirming your Donation... (please wait)</h1>
+                    <h2>Confirming your Donation... (please wait)</h2>
+                    <div className="svg-spin fa"><FaSpinner className="fa-spin"/></div>
                 </div>
             );
         }
         else if (this.state.confirmed) {
             jsx = (
                 <div>
+                    <h1 className="green"><FaCheck /></h1>
                     <h1>Thank You for Confirming your Donation!</h1>
                     <p>
                         Your donation offer has been confirmed.<br />
                         A PRFAA agent will be in touch with you regarding your donation.<br />
                         Thank you again!
                     </p>
-                    <Link to='/'>Home</Link>
+                    <Link className="btn btn-lg btn-donate" to='/'>Go Back to the Home Page</Link>
                 </div>
             );
         }
         else if (this.state.error) {
             jsx = (
                 <div>
+                    <div className="goX"><GoX/></div>
                     <h1>Sorry, we were unable to confirm your donation at this time.</h1>
                     <p>
-                        Please try again later or <Link to='/donation-offer'>resubmit your donation</Link>.
+                        Please try again later or <Link to='/donation-form'>resubmit your donation</Link>.
                     </p>
                 </div>
             );
         }
 
-        return jsx;
+        return (
+            <Grid fluid>
+                <Jumbotron>
+                    <Row>
+                        <Col xs={12} className="text-center">
+                            {jsx}
+                        </Col>
+                    </Row>
+                </Jumbotron>
+            </Grid>);
     }
 }
--- src/Search/components/login.js
+++ src/Search/components/login.js
@@ -4,6 +4,11 @@ import {
     Form,
     Input
 } from 'formsy-react-components';
+import {
+    Grid,
+    Button
+} from 'react-bootstrap';
+import './login.css';
 
 class Login extends Component {
     constructor(props) {
@@ -46,35 +51,41 @@ class Login extends Component {
 
     render() {
         return (
-            <Form id="signin" onValid={this.enableButton} onInvalid={this.disableButton} onValidSubmit={this.login}>
-                <p>This page is protected. Please sign in to continue.</p>
-                <Input
-                    name="username"
-                    label="Username"
-                    help="Enter your Username"
-                    placeholder="username"
-                    value={this.state.username}
-                    onChange={this.handleInputChange}
-                    required
-                    rowClassName="addMargin"
-                    labelClassName={[{'col-sm-3': false}, 'col-md-2 col-md-offset-2']}
-                    elementWrapperClassName={[{'col-sm-9': false}, 'col-md-6 col-xs-12']}
-                />
-                <Input
-                    type="password"
-                    name="password"
-                    label="Password"
-                    help="Enter your Password"
-                    placeholder="••••••••"
-                    value={this.state.password}
-                    onChange={this.handleInputChange}
-                    required
-                    rowClassName="addMargin"
-                    labelClassName={[{'col-sm-3': false}, 'col-md-2 col-md-offset-2']}
-                    elementWrapperClassName={[{'col-sm-9': false}, 'col-md-6 col-xs-12']}
-                />
-                <button type="submit" disabled={!this.state.canSubmit}>Submit</button>
-            </Form>
+            <div id="login-page">
+                <Grid>
+                    <Form id="signin" onValid={this.enableButton} onInvalid={this.disableButton} onValidSubmit={this.login}>
+                        <h3 className="text-center signin-header">This page is protected. <br/>Please sign in to continue.</h3>
+                        <Input
+                            name="username"
+                            label="Username"
+                            help="Enter your Username"
+                            placeholder="username"
+                            value={this.state.username}
+                            onChange={this.handleInputChange}
+                            required
+                            rowClassName="addMargin"
+                            labelClassName={[{'col-sm-3': false}, 'col-md-2 col-md-offset-2']}
+                            elementWrapperClassName={[{'col-sm-9': false}, 'col-md-6 col-xs-12']}
+                        />
+                        <Input
+                            type="password"
+                            name="password"
+                            label="Password"
+                            help="Enter your Password"
+                            placeholder="••••••••"
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                            required
+                            rowClassName="addMargin"
+                            labelClassName={[{'col-sm-3': false}, 'col-md-2 col-md-offset-2']}
+                            elementWrapperClassName={[{'col-sm-9': false}, 'col-md-6 col-xs-12']}
+                        />
+                        <Button type="submit" disabled={!this.state.canSubmit} className="col-xs-12 col-md-6 col-md-offset-3 btn-donate" type="submit" bsSize="large">
+                            Submit
+                        </Button>
+                    </Form>
+                </Grid>
+            </div>
         );
     }
 }
